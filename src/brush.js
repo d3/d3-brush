a952509e07965ead4a7756217e8854b5df09bccb
Mike Bostock <mbostock@gmail.com>
2016-05-26 16:45:44 -0700

    Add brush.move.

--- src/brush.js
+++ src/brush.js
@@ -1,23 +1,17 @@
 import {dispatch} from "d3-dispatch";
+import {customEvent} from "d3-selection";
 import constant from "./constant";
+import BrushEvent from "./event";
 
-var brushCursor = {
-  n: "ns-resize",
-  e: "ew-resize",
-  s: "ns-resize",
-  w: "ew-resize",
-  nw: "nwse-resize",
-  ne: "nesw-resize",
-  se: "nwse-resize",
-  sw: "nesw-resize"
-};
-
-var brushResizes = [
-  ["n", "e", "s", "w", "nw", "ne", "se", "sw"],
-  ["e", "w"],
-  ["n", "s"],
-  []
-];
+var N = {id: "n", cursor: "ns-resize"},
+    E = {id: "e", cursor: "ew-resize"},
+    S = {id: "s", cursor: "ns-resize"},
+    W = {id: "w", cursor: "ew-resize"},
+    NW = {id: "nw", cursor: "nwse-resize"},
+    NE = {id: "ne", cursor: "nesw-resize"},
+    SE = {id: "se", cursor: "nwse-resize"},
+    SW = {id: "sw", cursor: "nesw-resize"},
+    brushResizes = [[N, E, S, W, NW, NE, SE, SW], [E, W], [N, S], []];
 
 function defaultExtent() {
   var svg = this.ownerSVGElement;
@@ -28,7 +22,7 @@ function defaultExtent() {
 
 export default function() {
   var extent = defaultExtent,
-      listeners = dispatch(brush, "brushstart", "brush", "brushend"),
+      listeners = dispatch(brush, "start", "brush", "end"),
       resizes = brushResizes[0];
 
   // TODO tell the brush whether you can brush in x, y or x and y.
@@ -58,24 +52,55 @@ export default function() {
       .enter().append("rect")
         .attr("class", "extent")
         .attr("cursor", "move")
+        .attr("fill", "rgba(0,0,0,0.15)")
         .style("display", "none");
 
     var resize = selection.selectAll(".resize")
-      .data(resizes, String);
+      .data(resizes, function(d) { return d.id; });
 
     resize.exit().remove();
 
     resize.enter().append("g")
-        .attr("class", function(d) { return "resize resize--" + d; })
-        .attr("cursor", function(d) { return brushCursor[d]; })
+        .attr("class", function(d) { return "resize resize--" + d.id; })
+        .attr("cursor", function(d) { return d.cursor; })
         .style("display", "none")
       .append("rect")
-        .attr("x", function(d) { return /[ew]$/.test(d) ? -3 : null; })
-        .attr("y", function(d) { return /^[ns]/.test(d) ? -3 : null; })
+        .attr("x", function(d) { return /[ew]$/.test(d.id) ? -3 : null; })
+        .attr("y", function(d) { return /^[ns]/.test(d.id) ? -3 : null; })
         .attr("width", 6)
         .attr("height", 6)
         .attr("fill", "none");
   }
+
+  brush.move = function(selection, active) {
+    selection
+        .interrupt()
+        .property("__brush", active);
+
+    selection.select(".extent")
+        .datum(function() { return this.parentNode.__brush; })
+        .style("display", function(d) { return d == null ? "none" : null; })
+      .filter(function(d) { return d != null; })
+        .attr("x", function(d) { return d[0][0]; })
+        .attr("y", function(d) { return d[0][1]; })
+        .attr("width", function(d) { return d[1][0] - d[0][0]; })
+        .attr("height", function(d) { return d[1][1] - d[0][1]; });
+
+    selection.selectAll(".resize")
+        .each(function(d) { d.extent = this.parentNode.__brush; })
+        .style("display", function(d) { return d.extent == null ? "none" : null; })
+      .filter(function(d) { return d.extent != null; })
+        .attr("transform", function(d) { return "translate(" + d.extent[+/e$/.test(d.id)][0] + "," + d.extent[+/^s/.test(d.id)][1] + ")"; })
+      .select("rect")
+        .attr("width", function(d) { return /^(n|s)$/.test(d.id) ? d.extent[1][0] - d.extent[0][0] : 6; })
+        .attr("height", function(d) { return /^(e|w)$/.test(d.id) ? d.extent[1][1] - d.extent[0][1] : 6; });
+
+    selection.each(function() {
+      customEvent(new BrushEvent(brush, "start", this.__brush), listeners.apply, listeners, ["start", this, this]);
+      customEvent(new BrushEvent(brush, "brush", this.__brush), listeners.apply, listeners, ["brush", this, this]);
+      customEvent(new BrushEvent(brush, "end", this.__brush), listeners.apply, listeners, ["end", this, this]);
+    });
+  };
 
   brush.extent = function(_) {
     return arguments.length ? (extent = typeof _ === "function" ? _ : constant([[+_[0][0], +_[0][1]], [+_[1][0], +_[1][1]]]), brush) : extent;
---
+++ src/event.js
@@ -0,0 +1,5 @@
+export default function(target, type, active) {
+  this.target = target;
+  this.type = type;
+  this.active = active;
+}

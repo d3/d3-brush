732e98f5347ef27083e4923c93d25be03b0bcdcf
Mike Bostock <mbostock@gmail.com>
2016-05-31 13:02:04 -0700

    Cleaner internal state.
    Rather than use data (which can be messy, because maybe you’re apply the brush
    to a selection that already has data), use a technique like d3.local.

--- src/brush.js
+++ src/brush.js
@@ -3,29 +3,15 @@ import {customEvent} from "d3-selection";
 import constant from "./constant";
 import BrushEvent from "./event";
 
-var N = {id: "n", cursor: "ns-resize"},
-    E = {id: "e", cursor: "ew-resize"},
-    S = {id: "s", cursor: "ns-resize"},
-    W = {id: "w", cursor: "ew-resize"},
-    NW = {id: "nw", cursor: "nwse-resize"},
-    NE = {id: "ne", cursor: "nesw-resize"},
-    SE = {id: "se", cursor: "nwse-resize"},
-    SW = {id: "sw", cursor: "nesw-resize"},
-    brushResizes = [[N, E, S, W, NW, NE, SE, SW], [E, W], [N, S], []];
+var N = "n", E = "e", S = "s", W = "w", NW = "nw", NE = "ne", SE = "se", SW = "sw",
+    brushResizes = [[N, E, S, W, NW, NE, SE, SW], [E, W], [N, S], []],
+    brushCursors = {n: "ns", e: "ew", s: "ns", w: "ew", nw: "nwse", ne: "nesw", se: "nwse", sw: "nesw"};
 
 function defaultExtent() {
   var svg = this.ownerSVGElement;
   return [[0, 0], svg
       ? [svg.width.baseVal.value, svg.height.baseVal.value]
       : [this.clientWidth, this.clientHeight]];
-}
-
-function resizeData(d) {
-  return {
-    id: d.id,
-    cursor: d.cursor,
-    extent: null
-  };
 }
 
 export default function() {
@@ -38,35 +24,36 @@ export default function() {
   // TODO the initial render of the brush assumes that the selected extent is empty
   // TODO how do you update the extent of the background?
   function brush(selection) {
-    var background = selection.selectAll(".background")
-      .data(function() { return [extent.apply(this, arguments)]; });
+    var background = selection
+        .property("__brush", initialLocal)
+      .selectAll(".background")
+      .data([{type: "background"}]);
 
     background.enter().append("rect")
         .attr("class", "background")
         .attr("fill", "none")
         .attr("cursor", "crosshair")
       .merge(background)
-        .attr("x", function(d) { return d[0][0]; })
-        .attr("y", function(d) { return d[0][1]; })
-        .attr("width", function(d) { return d[1][0] - d[0][0]; })
-        .attr("height", function(d) { return d[1][1] - d[0][1]; });
+        .attr("x", function() { var l = local(this); return l.extent[0][0]; })
+        .attr("y", function() { var l = local(this); return l.extent[0][1]; })
+        .attr("width", function() { var l = local(this); return l.extent[1][0] - l.extent[0][0]; })
+        .attr("height", function() { var l = local(this); return l.extent[1][1] - l.extent[0][1]; });
 
-    selection.selectAll(".extent")
-      .data([null])
+    selection.selectAll(".selection")
+      .data([{type: "selection"}])
       .enter().append("rect")
-        .attr("class", "extent")
+        .attr("class", "selection")
         .attr("cursor", "move")
         .attr("fill", "rgba(0,0,0,0.15)");
 
     var resize = selection.selectAll(".resize")
-      .data(resizes.map(resizeData), function(d) { return d.id; });
+      .data(resizes.map(function(t) { return {type: t}; }), function(d) { return d.type; });
 
     resize.exit().remove();
 
-    resize.enter().append("g")
-        .attr("class", function(d) { return "resize resize--" + d.id; })
-        .attr("cursor", function(d) { return d.cursor; })
-      .append("rect")
+    resize.enter().append("rect")
+        .attr("class", function(d) { return "resize resize--" + d.type; })
+        .attr("cursor", function(d) { return brushCursors[d.type] + "-resize"; })
         .attr("fill", "none");
 
     selection
@@ -81,7 +68,9 @@ export default function() {
   brush.move = function(selection, selected) {
     selection
         .interrupt()
-        .property("__brush", selected)
+        .each(typeof selected === "function"
+            ? function() { this.__brush.selected = selected.apply(this, arguments); }
+            : function() { this.__brush.selected = selected; })
         .call(redraw)
         .call(emit, "start")
         .call(emit, "brush")
@@ -89,29 +78,39 @@ export default function() {
   };
 
   function redraw(selection) {
-    selection.select(".extent")
-        .datum(function() { return this.parentNode.__brush; })
-        .style("display", function(d) { return d == null ? "none" : null; })
-      .filter(function(d) { return d != null; })
-        .attr("x", function(d) { return d[0][0]; })
-        .attr("y", function(d) { return d[0][1]; })
-        .attr("width", function(d) { return d[1][0] - d[0][0]; })
-        .attr("height", function(d) { return d[1][1] - d[0][1]; });
+    selection.selectAll(".selection")
+        .style("display", function() { var l = local(this); return l.selected == null ? "none" : null; })
+      .filter(function() { var l = local(this); return l.selected != null; })
+        .attr("x", function() { var l = local(this); return l.selected[0][0]; })
+        .attr("y", function() { var l = local(this); return l.selected[0][1]; })
+        .attr("width", function() { var l = local(this); return l.selected[1][0] - l.selected[0][0]; })
+        .attr("height", function() { var l = local(this); return l.selected[1][1] - l.selected[0][1]; });
 
     selection.selectAll(".resize")
-        .each(function(d) { d.extent = this.parentNode.__brush; })
-        .style("display", function(d) { return d.extent == null ? "none" : null; })
-      .filter(function(d) { return d.extent != null; })
-        .attr("transform", function(d) { return "translate(" + d.extent[+/e$/.test(d.id)][0] + "," + d.extent[+/^s/.test(d.id)][1] + ")"; })
-      .select("rect")
-        .attr("width", function(d) { return /^(n|s)$/.test(d.id) ? d.extent[1][0] - d.extent[0][0] : 6; })
-        .attr("height", function(d) { return /^(e|w)$/.test(d.id) ? d.extent[1][1] - d.extent[0][1] : 6; });
+        .style("display", function() { var l = local(this); return l.selected == null ? "none" : null; })
+      .filter(function() { var l = local(this); return l.selected != null; })
+        .attr("x", function(d) { var l = local(this); return d.type[d.type.length - 1] === "e" ? l.selected[1][0] - 3 : l.selected[0][0] - 3; })
+        .attr("y", function(d) { var l = local(this); return d.type[0] === "s" ? l.selected[1][1] - 3 : l.selected[0][1] - 3; })
+        .attr("width", function(d) { var l = local(this); return d.type === "n" || d.type === "s" ? l.selected[1][0] - l.selected[0][0] + 6 : 6; })
+        .attr("height", function(d) { var l = local(this); return d.type === "e" || d.type === "w" ? l.selected[1][1] - l.selected[0][1] + 6 : 6; });
   }
 
   function emit(selection, type) {
     selection.each(function() {
       customEvent(new BrushEvent(brush, type, this.__brush), listeners.apply, listeners, [type, this, arguments]);
     });
+  }
+
+  function initialLocal() {
+    var local = this.__brush || {selected: null};
+    local.extent = extent.apply(this, arguments);
+    return local;
+  }
+
+  // Like d3.local, but with the name “__brush” rather than auto-generated.
+  function local(node) {
+    while (!node.__brush) if (!(node = node.parentNode)) return;
+    return node.__brush;
   }
 
   brush.extent = function(_) {
